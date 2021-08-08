b44b321d6d9ce29125e6c872fe23867d731d5cb7
Snowram <robin.gilh@gmail.com>
2021-08-08 16:15:46 +0200

    Arthropod run

--- voxygen/src/scene/figure/mod.rs
+++ voxygen/src/scene/figure/mod.rs
@@ -3427,7 +3427,7 @@ impl FigureMgr {
                                         StageSection::Buildup => {
                                             stage_time / stage.base_buildup_duration.as_secs_f32()
                                         },
-                                        StageSection::Swing => {
+                                        StageSection::Action => {
                                             stage_time / stage.base_swing_duration.as_secs_f32()
                                         },
                                         StageSection::Recover => {
